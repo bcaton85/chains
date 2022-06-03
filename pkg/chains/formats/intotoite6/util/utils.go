6555d338bb0bc88b9a2938e3886ba83f28e273b0
bcaton <bcaton@redhat.com>
2022-06-03 10:16:24 -0400

    Adding tests for new POC code

--- pkg/chains/formats/intotoite6/util/utils.go
+++ pkg/chains/formats/intotoite6/util/utils.go
@@ -1,7 +1,22 @@
+/*
+Copyright 2020 The Tekton Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package util
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"sort"
 	"strings"
 
@@ -148,4 +163,28 @@ func AttestInvocation(params []v1beta1.Param, paramSpecs []v1beta1.ParamSpec, lo
 
 	i.Parameters = iParams
 	return i
+}
+
+func TaskrunFromFile(f string) (*v1beta1.TaskRun, error) {
+	contents, err := ioutil.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+	var tr v1beta1.TaskRun
+	if err := json.Unmarshal(contents, &tr); err != nil {
+		return nil, err
+	}
+	return &tr, nil
+}
+
+func PipelinerunFromFile(f string) (*v1beta1.PipelineRun, error) {
+	contents, err := ioutil.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+	var pr v1beta1.PipelineRun
+	if err := json.Unmarshal(contents, &pr); err != nil {
+		return nil, err
+	}
+	return &pr, nil
 }
